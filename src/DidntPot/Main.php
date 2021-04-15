233485bb1bf5145edc59b746f08ac9e5bf58f746
DidntPot <79522460+DidntPot@users.noreply.github.com>
2021-04-15 03:13:47 -0700

    Bug Fixes.

--- src/DidntPot/Main.php
+++ src/DidntPot/Main.php
@@ -67,7 +67,7 @@ class Main extends PluginBase implements Listener {
 		$level = $player->getLevel();
 
 		$entity = Entity::createBaseNBT(new Vector3($player->getX(), $player->getY(), $player->getZ()));
-		$player->setMotion($player->getDirectionVector()->normalize()->multiply(2)); 
+		$entity->setMotion($player->getDirectionVector()->normalize()->multiply(2)); 
 		$fireball = new LargeFireball($player->level, $entity, $player);
 		$fireball->setExplode(true);
 		$launch = new ProjectileLaunchEvent($fireball);
